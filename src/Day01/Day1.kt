4a9f2186a931354b2d71fa44c25edc0883c36aa8
Will Goring <will@shadowrealm.org>
2024-12-03 01:20:22 +0000

    First two days

---
+++ src/Day01/Day1.kt
@@ -0,0 +1,43 @@
+package Day01
+
+import util.printTimedOutput
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    printTimedOutput("Puzzle 1 ") { puzzle1() }
+    printTimedOutput("Puzzle 2 ") { puzzle2() }
+}
+
+private fun puzzle1(): Int {
+    val (firstList, secondList) = loadLists()
+
+    firstList.sort()
+    secondList.sort()
+
+    val distance = firstList.indices.map { i -> abs(firstList[i] - secondList[i]) }.sum()
+
+    return distance
+}
+
+private fun puzzle2(): Int {
+    val (firstList, secondList) = loadLists()
+
+    val similarity = firstList.map { first ->
+        first * secondList.count { it == first }
+    }.sum()
+
+    return similarity
+}
+
+private fun loadLists(): Pair<MutableList<Int>, MutableList<Int>> {
+    val firstList = mutableListOf<Int>()
+    val secondList = mutableListOf<Int>()
+
+    File("input/day1.txt").forEachLine {
+        val (first, second) = it.split(Regex("\\s+")).map { it.toInt() }
+        firstList.add(first)
+        secondList.add(second)
+    }
+    return Pair(firstList, secondList)
+}
---
+++ src/Day02/Day2.kt
@@ -0,0 +1,50 @@
+package Day02
+
+import util.printTimedOutput
+import java.io.File
+
+fun main() {
+    printTimedOutput("Puzzle 1 ") { puzzle1() }
+    printTimedOutput("Puzzle 2 ") { puzzle2() }
+}
+
+private fun puzzle1(): Int {
+    val reports: List<List<Int>> = readReports()
+
+    val safe = reports.count { isSafe(it) }
+    return safe
+}
+
+private fun isSafe(it: List<Int>): Boolean {
+    if (it[0] == it[1]) return false
+
+    val values = if (it[0] < it[1]) it else it.reversed()
+
+    return values.indices.all { i ->
+        when {
+            i == 0 -> true
+            values[i] - values[i - 1] in 1..3 -> true
+            else -> false
+        }
+    }
+}
+
+private fun puzzle2(): Int {
+    val reports = readReports()
+
+    val safe = reports.count { isSafe2(it) }
+
+    return safe
+}
+
+private fun isSafe2(report: List<Int>): Boolean {
+    val permutations = report.indices.map { index -> report.filterIndexed {i, _ -> i != index} }
+    return permutations.any { isSafe(it) }
+}
+
+private fun readReports(): List<List<Int>> {
+    val reports: List<List<Int>> = File("input/day2.txt").readLines().map {
+        it.split(" ").map { it.toInt() }
+    }
+    return reports
+}
