03fdb6f2974078b1c75716181dbfeac4342ae867
Will Goring <will@shadowrealm.org>
2024-12-11 13:42:32 +0000

    Day 11 part 2

--- src/Day11/Day11.kt
+++ src/Day11/Day11.kt
@@ -4,33 +4,38 @@ import util.printTimedOutput
 import java.io.File
 
 fun main() {
-    printTimedOutput("Puzzle 1 test") { puzzle1("input/day11-test.txt") }
-    printTimedOutput("Puzzle 1     ") { puzzle1("input/day11.txt") }
-//    printTimedOutput("Puzzle 2 test") { puzzle2("input/day11-test.txt") }
-//    printTimedOutput("Puzzle 2     ") { puzzle2("input/day11.txt") }
+    printTimedOutput("Puzzle 1 test") { puzzle("input/day11-test.txt", 25) }
+    printTimedOutput("Puzzle 1     ") { puzzle("input/day11.txt", 25) }
+    printTimedOutput("Puzzle 2 test") { puzzle("input/day11-test.txt", 75) }
+    printTimedOutput("Puzzle 2     ") { puzzle("input/day11.txt", 75) }
 }
 
-fun puzzle1(fileName: String): Int {
-    var stones = File(fileName).readLines()[0].split(' ').map { it.toLong() }
+val cache: MutableMap<Pair<Long, Int>, Long> = mutableMapOf()
 
-    repeat(25) {
-        stones = stones.flatMap { stone ->
-            when {
-                stone == 0L -> listOf(1L)
-                stone.toString().length.isEven() -> listOf(
-                    stone.toString().substring(0, stone.toString().length / 2).toString().toLong(),
-                    stone.toString().substring(stone.toString().length / 2).toString().toLong()
-                )
-                else -> listOf(stone * 2024L)
-            }
+fun puzzle(fileName: String, blinks: Int): Long {
+    val stones = File(fileName).readLines()[0].split(' ').map { it.toLong() }
+
+    return stones.map { stone -> getFinalStoneCountForStone(stone, blinks) }.sum()
+}
+
+fun getFinalStoneCountForStone(stone: Long, blinks: Int): Long {
+    cache[stone to blinks]?.let { return it }
+
+    if (blinks == 0) return 1
+
+    val stoneString = stone.toString()
+    return when {
+        stone == 0L -> getFinalStoneCountForStone(1, blinks - 1)
+        stoneString.length.isEven() -> {
+            val half = stoneString.length / 2
+            val left = stoneString.substring(0, half).toLong()
+            val right = stoneString.substring(half).toLong()
+            getFinalStoneCountForStone(left, blinks - 1) + getFinalStoneCountForStone(right, blinks - 1)
         }
+        else -> getFinalStoneCountForStone(stone * 2024, blinks - 1)
+    }.also {
+        cache[stone to blinks] = it
     }
-
-    return stones.size
 }
 
 private fun Int.isEven(): Boolean = this % 2 == 0
-
-fun puzzle2(fileName: String): Int {
-    return -1
-}
