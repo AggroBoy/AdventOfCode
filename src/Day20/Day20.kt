66824fb207eac7f5c9ba9a94958ecc945ed0d172
Will Goring <will@shadowrealm.org>
2024-12-22 22:42:57 +0000

    Refactoring/renaming

--- src/Day20/Day20.kt
+++ src/Day20/Day20.kt
@@ -13,12 +13,6 @@ fun main() {
     printTimedOutput("Puzzle 2     ") { puzzle2("input/day20.txt", 100) }
 }
 
-typealias Course = Array<Array<Boolean>>
-fun Course.get(location: Coord) = this[location.y.toInt()][location.x.toInt()]
-fun Course.set(location: Coord, value: Boolean) { this[location.y.toInt()][location.x.toInt()] = value }
-
-fun Course(x: Int, y: Int) = Array<Array<Boolean>>(size = y, init = { Array(size = x, init = { false }) })
-
 fun puzzle1(fileName: String, threshold: Int): Int = solvePuzzle(fileName, threshold, 2)
 fun puzzle2(fileName: String, threshold: Int): Int = solvePuzzle(fileName, threshold, 20)
 
@@ -52,7 +46,6 @@ private fun solvePuzzle(fileName: String, threshold: Int, cheatDuration : Int): 
     var targetLocation = Coord(0, 0)
 
     val lines = File(fileName).readLines()
-    //val course = Course(lines[0].length, lines.size)
     val walls: MutableSet<Coord> = mutableSetOf()
 
     lines.mapIndexed { y, line ->
@@ -65,28 +58,23 @@ private fun solvePuzzle(fileName: String, threshold: Int, cheatDuration : Int): 
 
     val route = mapRouteTimings(walls, startLocation, targetLocation)
 
-    val possibleCheats = getPossibleCheatsForDuration(cheatDuration)
-
-    val cheats: MutableMap<Pair<Coord, Coord>, Int> = mutableMapOf()
+    val possibleCheatDeltas = getPossibleCheatsForDuration(cheatDuration)
+    val possibleCheats: MutableMap<Pair<Coord, Coord>, Int> = mutableMapOf()
 
     route.map { cheatStart ->
-        possibleCheats.map { cheat ->
+        possibleCheatDeltas.map { cheat ->
             cheatStart.key + cheat to cheat.manhattanDistanceFrom(Coord(0, 0)).toInt()
         }.filter { (cheatEnd, _) ->
             route.contains(cheatEnd)
         }.map { (cheatEnd, cheatDuration) ->
             val saving = route[cheatEnd]!! - cheatStart.value - cheatDuration
             if (saving >= threshold) {
-                cheats[cheatStart.key to cheatEnd] = saving
+                possibleCheats[cheatStart.key to cheatEnd] = saving
             }
         }
     }
 
-//    val totals = cheats.values.toSet().map { saving ->
-//        saving to cheats.count { it.value == saving }
-//    }.sortedBy { it.first }
-
-    return cheats.count()
+    return possibleCheats.count()
 }
 
 private fun getPossibleCheatsForDuration(cheatDuration: Int) =
