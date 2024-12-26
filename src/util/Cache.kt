07799838963f57f5060c9a8e3d9cc6f84398c750
Will Goring <will@shadowrealm.org>
2024-12-26 14:55:24 +0000

    Improve the cache
    This makes it more useful for it’s intended use of memoization, since being able to usefully store null (which it didn’t before, and Maps aren’t great at) is very useful in that case.

--- src/util/Cache.kt
+++ src/util/Cache.kt
@@ -1,11 +1,28 @@
 package util
 
+// This is a simple cache designed to be used for memoization of
+// function calls or simple code blocks. It's not thread safe, and
+// doesn't really do anything you can't do with a Map, but it's
+// null-safe, which is useful for memoization, and has a nice syntax.
+// Almost all interactions should be getOrStore()
 class Cache<K, V> {
     val map: MutableMap<K, V> = mutableMapOf()
 
     fun getOrStore(key: K, lambda: (K) -> V): V {
-        return map[key] ?: lambda(key).also { map[key] = it }
+        try {
+            // Use getValue because it throws, so we can tell the difference
+            // between a null value and a missing value
+            return map.getValue(key)
+        } catch (e: NoSuchElementException) {
+            return lambda(key).also { map.put(key, it) }
+        }
     }
 
+    fun get(key: K): V = map.getValue(key)
+    fun put(key: K, value: V) {
+        map[key] = value
+    }
+    fun contains(key: K): Boolean = map.containsKey(key)
+    fun remove(key: K) = map.remove(key)
     fun flush() = map.clear()
 }
