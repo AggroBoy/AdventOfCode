afb925a2cff27c16f385404831b7059d8043b40c
Will Goring <will@shadowrealm.org>
2024-12-27 02:09:07 +0000

    2022 day 1

---
+++ src/twenty15/day01/Day27.kt
@@ -0,0 +1,26 @@
+package twenty22.day01
+
+import util.printTimedOutput
+import java.io.File
+
+fun main() {
+    printTimedOutput("Puzzle 1     ") { puzzle1("input/2015/day1.txt") }
+    printTimedOutput("Puzzle 2     ") { puzzle2("input/2015/day1.txt") }
+}
+
+fun puzzle1(fileName: String): Int {
+    var instructions = File(fileName).readLines().joinToString("")
+
+    return instructions.count { it == '(' } - instructions.count { it == ')' }
+}
+
+fun puzzle2(fileName: String): Int {
+    var instructions = File(fileName).readLines().joinToString("")
+
+    var floor = 0
+    instructions.forEachIndexed { index, c ->
+        floor += if (c == '(') 1 else -1
+        if (floor == -1) return index + 1
+    }
+    return -1
+}
