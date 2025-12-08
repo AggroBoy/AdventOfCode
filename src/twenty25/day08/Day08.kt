8b9ecbee64801b66248334ace369b7f47aea42fa
Will Goring <will@goring.zone>
2025-12-08 09:28:26 +0000

    Remove duplicated set-up

--- src/twenty25/day08/Day08.kt
+++ src/twenty25/day08/Day08.kt
@@ -11,7 +11,7 @@ fun main() {
     printTimedOutput("Puzzle 2     ") { puzzle2("input/2025/day08.txt") }
 }
 
-fun puzzle1(fileName: String, iterations: Int): Long {
+class Playground(fileName: String) {
     val boxes: List<Coord3d> = File(fileName)
         .readLines()
         .map { it.split(",") }
@@ -21,46 +21,46 @@ fun puzzle1(fileName: String, iterations: Int): Long {
     }.associate { it.first.disatanceFrom(it.second) to it}
     val sortedDistances = pairDistances.keys.sorted()
     val circuits = boxes.map { mutableListOf(it) }.toMutableList()
+}
 
-    sortedDistances.subList(0, iterations).forEach { distance ->
-        val boxOne = pairDistances[distance]?.first ?: throw IllegalArgumentException("Nope")
-        val circuitOne = circuits.find { it.contains(boxOne) } ?: throw IllegalArgumentException("Nope")
-        val boxTwo = pairDistances[distance]?.second ?: throw IllegalArgumentException("Nope")
-        val circuitTwo = circuits.find { it.contains(boxTwo) } ?: throw IllegalArgumentException("Nope")
+fun puzzle1(fileName: String, iterations: Int): Long {
+    val playground = Playground(fileName)
 
-        if (circuitOne != circuitTwo) {
-            circuitOne.addAll(circuitTwo)
-            circuits.remove(circuitTwo)
+    with(playground) {
+        sortedDistances.subList(0, iterations).forEach { distance ->
+            val boxOne = pairDistances[distance]?.first ?: throw IllegalArgumentException("Nope")
+            val circuitOne = circuits.find { it.contains(boxOne) } ?: throw IllegalArgumentException("Nope")
+            val boxTwo = pairDistances[distance]?.second ?: throw IllegalArgumentException("Nope")
+            val circuitTwo = circuits.find { it.contains(boxTwo) } ?: throw IllegalArgumentException("Nope")
+
+            if (circuitOne != circuitTwo) {
+                circuitOne.addAll(circuitTwo)
+                circuits.remove(circuitTwo)
+            }
         }
-    }
 
-    return circuits.map { it.size }.sortedDescending().subList(0, 3).reduce{ acc, size -> acc * size }.toLong()
+        return circuits.map { it.size }.sortedDescending().subList(0, 3).reduce { acc, size -> acc * size }.toLong()
+    }
 }
 
 fun puzzle2(fileName: String): Long {
-    val boxes: List<Coord3d> = File(fileName)
-        .readLines()
-        .map { it.split(",") }
-        .map { Coord3d(it[0].toLong(), it[1].toLong(), it[2].toLong()) }
-    val pairDistances = boxes.flatMap { one ->
-        boxes.filter { it != one }.map { one to it }
-    }.associate { it.first.disatanceFrom(it.second) to it }
-    val sortedDistances = pairDistances.keys.sorted()
-    val circuits = boxes.map { mutableListOf(it) }.toMutableList()
+    val playground = Playground(fileName)
 
-    sortedDistances.forEach { distance ->
-        val boxOne = pairDistances[distance]?.first ?: throw IllegalArgumentException("Nope")
-        val circuitOne = circuits.find { it.contains(boxOne) } ?: throw IllegalArgumentException("Nope")
-        val boxTwo = pairDistances[distance]?.second ?: throw IllegalArgumentException("Nope")
-        val circuitTwo = circuits.find { it.contains(boxTwo) } ?: throw IllegalArgumentException("Nope")
+    with (playground) {
+        sortedDistances.forEach { distance ->
+            val boxOne = pairDistances[distance]?.first ?: throw IllegalArgumentException("Nope")
+            val circuitOne = circuits.find { it.contains(boxOne) } ?: throw IllegalArgumentException("Nope")
+            val boxTwo = pairDistances[distance]?.second ?: throw IllegalArgumentException("Nope")
+            val circuitTwo = circuits.find { it.contains(boxTwo) } ?: throw IllegalArgumentException("Nope")
 
-        if (circuitOne != circuitTwo) {
-            if (circuits.size == 2) {
-                return (boxOne.x * boxTwo.x)
+            if (circuitOne != circuitTwo) {
+                if (circuits.size == 2) {
+                    return (boxOne.x * boxTwo.x)
+                }
+                circuitOne.addAll(circuitTwo)
+                circuits.remove(circuitTwo)
             }
-            circuitOne.addAll(circuitTwo)
-            circuits.remove(circuitTwo)
         }
+        throw Exception("Ran out!")
     }
-    throw Exception("Ran out!")
 }
