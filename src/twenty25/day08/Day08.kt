4bb79ae401a6787c5db7ab29ac0ec1c0edb00884
Will Goring <will@goring.zone>
2025-12-08 09:20:09 +0000

    2025 day 8 Solution
    And added a 3d coordinate helper class, for working with coordinates in 3d space.

---
+++ src/twenty25/day08/Day08.kt
@@ -0,0 +1,66 @@
+package twenty25.day08
+
+import util.Coord3d
+import util.printTimedOutput
+import java.io.File
+
+fun main() {
+    printTimedOutput("Puzzle 1 test") { puzzle1("input/2025/day08-test.txt", 10) }
+    printTimedOutput("Puzzle 1     ") { puzzle1("input/2025/day08.txt", 1000) }
+    printTimedOutput("Puzzle 2 test") { puzzle2("input/2025/day08-test.txt") }
+    printTimedOutput("Puzzle 2     ") { puzzle2("input/2025/day08.txt") }
+}
+
+fun puzzle1(fileName: String, iterations: Int): Long {
+    val boxes: List<Coord3d> = File(fileName)
+        .readLines()
+        .map { it.split(",") }
+        .map { Coord3d(it[0].toLong(), it[1].toLong(), it[2].toLong()) }
+    val pairDistances = boxes.flatMap { one ->
+        boxes.filter { it != one }.map { one to it }
+    }.associate { it.first.disatanceFrom(it.second) to it}
+    val sortedDistances = pairDistances.keys.sorted()
+    val circuits = boxes.map { mutableListOf(it) }.toMutableList()
+
+    sortedDistances.subList(0, iterations).forEach { distance ->
+        val boxOne = pairDistances[distance]?.first ?: throw IllegalArgumentException("Nope")
+        val circuitOne = circuits.find { it.contains(boxOne) } ?: throw IllegalArgumentException("Nope")
+        val boxTwo = pairDistances[distance]?.second ?: throw IllegalArgumentException("Nope")
+        val circuitTwo = circuits.find { it.contains(boxTwo) } ?: throw IllegalArgumentException("Nope")
+
+        if (circuitOne != circuitTwo) {
+            circuitOne.addAll(circuitTwo)
+            circuits.remove(circuitTwo)
+        }
+    }
+
+    return circuits.map { it.size }.sortedDescending().subList(0, 3).reduce{ acc, size -> acc * size }.toLong()
+}
+
+fun puzzle2(fileName: String): Long {
+    val boxes: List<Coord3d> = File(fileName)
+        .readLines()
+        .map { it.split(",") }
+        .map { Coord3d(it[0].toLong(), it[1].toLong(), it[2].toLong()) }
+    val pairDistances = boxes.flatMap { one ->
+        boxes.filter { it != one }.map { one to it }
+    }.associate { it.first.disatanceFrom(it.second) to it }
+    val sortedDistances = pairDistances.keys.sorted()
+    val circuits = boxes.map { mutableListOf(it) }.toMutableList()
+
+    sortedDistances.forEach { distance ->
+        val boxOne = pairDistances[distance]?.first ?: throw IllegalArgumentException("Nope")
+        val circuitOne = circuits.find { it.contains(boxOne) } ?: throw IllegalArgumentException("Nope")
+        val boxTwo = pairDistances[distance]?.second ?: throw IllegalArgumentException("Nope")
+        val circuitTwo = circuits.find { it.contains(boxTwo) } ?: throw IllegalArgumentException("Nope")
+
+        if (circuitOne != circuitTwo) {
+            if (circuits.size == 2) {
+                return (boxOne.x * boxTwo.x)
+            }
+            circuitOne.addAll(circuitTwo)
+            circuits.remove(circuitTwo)
+        }
+    }
+    throw Exception("Ran out!")
+}
--- src/util/Coord.kt
+++ src/util/Coord.kt
@@ -1,5 +1,7 @@
 package util
 
+import kotlin.math.absoluteValue
+
 //typealias Coord = Pair<Long, Long>
 data class Coord(val x: Long, val y: Long) {
     constructor(x: Int, y: Int) : this(x.toLong(), y.toLong())
@@ -12,4 +14,27 @@ data class Coord(val x: Long, val y: Long) {
     fun manhattanDistanceFrom(other: Coord) = Math.abs(this.x - other.x) + Math.abs(this.y - other.y)
 
     override fun toString() = "$x,$y"
+}
+
+fun Long.squared(): Long = this * this
+fun Long.sqroot(): Double = Math.sqrt(this.toDouble())
+
+data class Coord3d(val x: Long, val y: Long, val z: Long) {
+    constructor(x: Int, y: Int, z: Int) : this(x.toLong(), y.toLong(), z.toLong())
+
+    operator fun plus(other: Coord3d) = Coord3d(this.x + other.x, this.y + other.y, this.z + other.z)
+    operator fun minus(other: Coord3d) = Coord3d(this.x - other.x, this.y - other.y, z=this.z - other.z)
+
+    operator fun times(scalar: Long) = Coord3d(this.x * scalar, this.y * scalar, this.z * scalar)
+
+//    fun manhattanDistanceFrom(other: Coord3d) = Math.abs(this.x - other.x) + Math.abs(this.y - other.y)
+    fun disatanceFrom(other: Coord3d): Double {
+        return (
+            (this.x - other.x).absoluteValue.squared() +
+            (this.y - other.y).absoluteValue.squared() +
+            (this.z - other.z).absoluteValue.squared()
+        ).sqroot()
+    }
+
+    override fun toString() = "$x,$y,$z"
 }
