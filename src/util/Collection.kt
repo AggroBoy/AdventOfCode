29c36e08db66b8b57d9ce09ee8e52f8414bde8e9
Will Goring <will@goring.zone>
2026-01-01 14:08:37 +0000

    combine collection extensions

--- src/util/Collection.kt
+++ src/util/Collection.kt
@@ -16,3 +16,23 @@ fun <T> Collection<T>.permutations(): List<List<T>> {
 
     return result.sortedBy { it.size }
 }
+
+fun <T>List<T>.dropLast(): List<T> = this.dropLast(1)
+
+fun <T>List<T>.split(delimiter: T): List<List<T>> {
+    val newList = mutableListOf<List<T>>()
+    var subList = mutableListOf<T>()
+
+    this.forEach {
+        if (it == delimiter) {
+            newList.add(subList)
+            subList = mutableListOf()
+        } else {
+            subList.add(it)
+        }
+    }
+    if (newList.last() != subList)
+        newList.add(subList)
+
+    return newList
+}
--- src/util/Util.kt
+++ src/util/Util.kt
@@ -18,22 +18,3 @@ fun Int.times(lambda: () -> Unit) {
     (0 until this).forEach { lambda() }
 }
 
-fun <T>List<T>.dropLast(): List<T> = this.dropLast(1)
-
-fun <T>List<T>.split(delimiter: T): List<List<T>> {
-    val newList = mutableListOf<List<T>>()
-    var subList = mutableListOf<T>()
-
-    this.forEach {
-        if (it == delimiter) {
-            newList.add(subList)
-            subList = mutableListOf()
-        } else {
-            subList.add(it)
-        }
-    }
-    if (newList.last() != subList)
-        newList.add(subList)
-
-    return newList
-}
